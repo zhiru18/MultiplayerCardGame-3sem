e18354930405a1221a461aeeb88f9ef0359519a0
Zhiru Cao <1074160@ucn.dk>
2019-11-08 12:42:53 +0100

    oprettet loginservice med Login metode og isValidUser metode

---
+++ MultiplayerCardGame/UserManagementService/UsermanagementService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace UserManagementService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class UserManagementService : IUserManagementService
+    {
+        public UserManagementService() {
+
+        }
+        public UserManagementService(IUserRepository userRepository) {
+            _UserRepository = userRepository;
+        }
+        IUserRepository _UserRepository = null;
+        string userToken = string.Empty;
+        public string Login(string userName, string password) {
+            IUserRepository userRepository = _UserRepository ?? new UserRepository();
+            User user = userRepository.GetUserByUserName(userName);
+            if (user != null) {
+                if (password.Equals(user.Password)) {
+                    userToken = OperationContext.Current.SessionId;
+                } else {
+                    userToken = "";
+                } 
+            } else {
+                userToken = "";
+            }
+            return userToken;
+        }
+
+        public bool IsValidUser() {
+            bool isValid = false;
+            if (OperationContext.Current.IncomingMessageHeaders.FindHeader("TokenHeader", "TokenNameSpace") == -1) {
+                isValid = false;
+            } else {
+                string userIdentityToken = Convert.ToString(OperationContext.Current.IncomingMessageHeaders.GetHeader<string>("TokenHeader", "TokenNameSpace"));
+                if (userIdentityToken == userToken) {
+                    isValid = true;
+                } else {
+                    isValid = false;
+                }
+            }
+            return isValid;
+        }
+    }
+}
--- MultiplayerCardGame/UserManagementServiceTest/UnitTest1.cs
+++ MultiplayerCardGame/UserManagementServiceTest/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
 
 namespace UserManagementServiceTest
 {
@@ -7,8 +10,84 @@ namespace UserManagementServiceTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
-        {
+        public void TestSuccessfullLogin() {
+            //Arrange
+            Mock<IUserRepository> mockUserRepository = new Mock<IUserRepository>();
+            User user = new User() {
+                UserName = "Test",
+                PassWord = "1234",
+            };
+
+            //Act
+            mockUserRepository.Setup(obj => obj.GetByUserName("Test")).Returns(user);
+            IUserManagementService userManagementService = new UserManagementService(mockUserRepository.Object);
+            string data = userManagementService.Login("Test", "1234");
+
+            //Assert
+            Assert.AreNotEqual(data, "");
+
         }
+        [TestMethod]
+        public void TestUnssuccessfullLogin() {
+            //Arrange
+            Mock<IUserRepository> mockUserRepository = new Mock<IUserRepository>();
+            User user = new User() {
+                UserName = "Test",
+                PassWord = "1234",
+            };
+
+            //Act
+            mockUserRepository.Setup(obj => obj.GetByUserName("Test")).Returns(user);
+            IUserManagementService userManagementService = new UserManagementService(mockUserRepository.Object);
+            string data = userManagementService.Login("Test1", "1235");
+
+            //Assert
+            Assert.AreEqual(data, "");
+        }
+        [TestMethod]
+        public void TestIsValidUser() {
+            //Arrange
+            Mock<IUserRepository> mockUserRepository = new Mock<IUserRepository>();
+            User user = new User() {
+                UserName = "Test",
+                PassWord = "1234",
+            };
+            MessageHeader header;
+            string userToken;
+            bool isValid;
+            //Act
+            mockUserRepository.Setup(obj => obj.GetByUserName("Test")).Returns(user);
+            IUserManagementService userManagementService = new UserManagementService(mockUserRepository);
+            userToken = userManagementService.Login("Test", "1234");
+            header = MessageHeader.CreateHeader("TokenHeader", "TokenNameSpace", userToken);
+            OperationContext.Current.OutgoingMessageHeaders.Add(header);
+            isValid = userManagementService.IsValidUser();
+
+            //Assert
+            Assert.IsTrue(isValid);
+        }
+        [TestMethod]
+        public void TestIsInvalidUser() {
+            //Arrange
+            Mock<IUserRepository> mockUserRepository = new Mock<IUserRepository>();
+            User user = new User() {
+                UserName = "Test",
+                PassWord = "1234",
+            };
+            MessageHeader header;
+            string userToken;
+            bool isValid;
+            //Act
+            mockUserRepository.Setup(obj => obj.GetByUserName("Test")).Returns(user);
+            IUserManagementService userManagementService = new UserManagementService(mockUserRepository);
+            userToken = userManagementService.Login("Test1", "12345");
+            header = MessageHeader.CreateHeader("TokenHeader", "TokenNameSpace", userToken);
+            OperationContext.Current.OutgoingMessageHeaders.Add(header);
+            isValid = userManagementService.IsValidUser();
+
+            //Assert
+            Assert.IsFalse(isValid);
+        }
+
     }
 }
