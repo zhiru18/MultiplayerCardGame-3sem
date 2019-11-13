0eb6f3bf5d5b499af6d88cffed3ce2b6175ec451
Emil Hedeholm Sørensen <1074195@ucn.dk>
2019-11-13 12:33:10 +0100

    added webClient

---
+++ MultiplayerCardGame/WebGameClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebGameClient.Startup))]
+namespace WebGameClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
