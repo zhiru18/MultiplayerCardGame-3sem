8b43eda80903de916843072697dd6b8d0511dadf
mortenmkristensen <33622440+mortenmkristensen@users.noreply.github.com>
2019-11-13 12:37:06 +0100

    Updated the User class with attributes.

--- MultiplayerCardGame/Model/User.cs
+++ MultiplayerCardGame/Model/User.cs
@@ -5,6 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Model {
-    class User {
+    public class User {
+        public enum UserType { PLAYER, ADMIN };
+        public enum UserStatus { UNATTACKABLE, ATTACKABLE, STUNNED, INGAME };
+
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public UserType userType { get; set; }
+        public UserStatus userStatus { get; set; }
+
+        public User(string id, string userName, string email, string password, UserType userType, UserStatus userStatus) {
+            this.Id = id;
+            this.UserName = UserName;
+            this.Email = email;
+            this.Password = password;
+            this.userType = userType;
+            this.userStatus = userStatus;
+        }
+        public User() {
+
+        }
     }
 }
