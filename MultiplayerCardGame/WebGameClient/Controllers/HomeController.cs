6add2949b9a7bb6533ef3b5375ef540083761152
Emil Hedeholm Sørensen <1074195@ucn.dk>
2019-11-18 09:30:01 +0100

    tilføjet authorize på homecontroller

--- MultiplayerCardGame/WebGameClient/Controllers/HomeController.cs
+++ MultiplayerCardGame/WebGameClient/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace WebGameClient.Controllers {
-   // [Authorize]
+    [Authorize]
     public class HomeController : Controller {
         
         public ActionResult Index() {
