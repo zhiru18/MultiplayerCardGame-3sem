f2831b801ccf1b62c660950ed8f5486044acca08
Heidi Kim Thomsen <1074310@ucn.dk>
2019-11-22 14:26:22 +0100

    GameController lavet og startet på metoden ShuffleDeck deri.

---
+++ MultiplayerCardGame/Controller/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Server.Controllers.Controller {
+    public class GameController {
+    }
+
+    public Deck ShuffleDeck(Deck inputDeck) {
+        Deck outputDeck = new Deck();
+        Random random = new Random();
+        int randomIndex = 0;
+        while(inputDeck.cards.count > 0) {
+            //random tager et tilfældigt index som ligger imellem start og slut af listen cards. Altså den vælger et random object i listen. 
+            randomIndex = random.Next(0.inputDeck.cards.count);
+            //Objektet randomIndex bliver tilføjet til outputDecket. 
+            outputDeck.cards.Add(inputDeck.cards[randomIndex]);
+            //fjerner objektet igen fra inputlisten. 
+            inputDeck.cards.RemoveAdd(randomIndex);
+        }
+
+    }
+}
