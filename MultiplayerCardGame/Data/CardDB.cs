3d7f2c492fe2c0836901c3b04db5803477a9ec9a
Heidi Kim Thomsen <1074310@ucn.dk>
2019-11-22 13:30:30 +0100

    Card Model klasse (Server) og CardDB lavet.

---
+++ MultiplayerCardGame/Data/CardDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Data.Data {
+    public class CardDB {
+        private string conString;
+
+        public CardDB() {
+            conString = ConfigurationManager.ConnectionStrings["Con"].ConnectionString;
+        }
+    }
+}
---
+++ MultiplayerCardGame/Model/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Model.Model {
+    public class Card {
+
+        public enum CardType { ATTACK, DEFENSE, EFFECT}; 
+        public CardType cardtype { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Value { get; set; }
+
+        public Card(CardType cardType, int id, string name, string description, int value) {
+            this.cardtype = cardType;
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+            this.Value = value;
+        }
+    }
+}
