81c6af34502b9df2b46d7496963e3685ded6d0dd
Peanguin <Peanguin@users.noreply.github.com>
2019-11-25 09:24:37 +0100

    Added Card DBIF class and added functionality to CardDB

---
+++ MultiplayerCardGame/Data/ICardDBIF.cs
@@ -0,0 +1,14 @@
+﻿using Server.Data.Data;
+using Server.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Data.Data
+{
+    public interface ICardDBIF:IGeneralDBIF<Card>{
+
+    }
+}
