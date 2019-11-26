06a6aa16daec5a5bda5028df2dcb2aba5ec76ca4
Zhiru Cao <1074160@ucn.dk>
2019-11-26 10:19:31 +0100

    DeckDB, DeckDBTest, GameTableDBTest

---
+++ MultiplayerCardGame/Data/DeckDB.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Server.Model.Model;
+
+namespace Server.Data.Data {
+    public class DeckDB : IDeckDBIF {
+        private string conString;
+
+        public DeckDB() {
+            conString = "Server=tcp:cardgameucn.database.windows.net,1433;Initial Catalog=CardGameDB;Persist Security Info=False;User ID=gameadmin;Password=Bamsesjul1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            //conString = ConfigurationManager.ConnectionStrings["Con"].ConnectionString;
+        }
+
+        public void Delete(Deck deck) {
+            using (SqlConnection connection = new SqlConnection(conString)) {
+                connection.Open();
+                var sql = "DELETE FROM Deck WHERE id = @id;";
+                connection.Execute(sql, deck);
+            }
+        }
+
+        public IEnumerable<Deck> GetAll() {
+            using (SqlConnection connection = new SqlConnection(conString)) {
+                connection.Open();
+                return connection.Query<Deck>("SELECT Id, deckName FROM Deck").ToList();
+            }
+        }
+
+        public Deck GetById(int id) {
+            using (SqlConnection connection = new SqlConnection(conString)) {
+                connection.Open();
+                return connection.Query<Deck>("SELECT Id, deckName FROM Deck WHERE id = @id", new { id }).SingleOrDefault();
+            }
+        }
+
+        public void Insert(Deck deck) {
+            using (SqlConnection connection = new SqlConnection(conString)) {
+                       connection.Open();
+                       var sql = "INSERT INTO Deck (deckName) VALUES (@deckName);";
+                       connection.Execute(sql, deck);
+                }
+            }
+
+        public void Update(Deck deck) {
+            using (SqlConnection connection = new SqlConnection(conString)) {
+                connection.Open();
+                var sql = "UPDATE Deck SET deckName = @deckName WHERE id = @id;";
+                connection.Execute(sql, deck);
+            }
+        }
+    }    
+}
---
+++ MultiplayerCardGame/Data/IDeckDBIF.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server.Model.Model;
+
+namespace Server.Data.Data {
+    public interface IDeckDBIF : IGeneralDBIF<Deck> {
+       Deck GetById(int id);
+    }
+}
