6a57d4296f1c3f59c15f12116c7920258dbd71e3
Heidi Kim Thomsen <1074310@ucn.dk>
2019-11-27 12:17:42 +0100

    Oprettet tableController og View dertil

---
+++ MultiplayerCardGame/WebGameClient/Controllers/TableController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebGameClient.Controllers
+{
+    public class TableController : Controller
+    {
+        // GET: Table
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
