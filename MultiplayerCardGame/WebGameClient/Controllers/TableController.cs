50467ced3f933cb3ba57ce40fd845716750cbfb1
Zhiru Cao <1074160@ucn.dk>
2019-11-29 09:22:48 +0100

    join

--- MultiplayerCardGame/WebGameClient/Controllers/TableController.cs
+++ MultiplayerCardGame/WebGameClient/Controllers/TableController.cs
@@ -55,7 +55,7 @@ namespace WebGameClient.Controllers {
             return View();
         }
 
-        public ActionResult Join() {
+        public ActionResult JoinTable() {
             return View();
         }
     }
