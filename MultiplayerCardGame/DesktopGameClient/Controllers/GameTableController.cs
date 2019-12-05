951c3c5ecc1c9376ac31c40083d2c853fe0e9320
Zhiru Cao <1074160@ucn.dk>
2019-12-05 13:04:59 +0100

    gametable service controller, gametablegui

---
+++ MultiplayerCardGame/DesktopGameClient/Controllers/GameTableController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesktopGameClient.Models;
+using DesktopGameClient.ServiceAcces;
+
+namespace DesktopGameClient.Controllers {
+    public class GameTableController {
+
+        GameTableServiceAccess gameTableAccess = new GameTableServiceAccess();
+
+        public List<GameTableModel> GetAll() {
+            return gameTableAccess.GetAll();
+        }
+
+        public GameTableModel GetById(int tableId) {
+            return gameTableAccess.GetById(tableId);
+        }
+
+        public bool Delete(int tableId) {
+            return gameTableAccess.Delete(tableId);
+        }
+    }
+}
---
+++ MultiplayerCardGame/DesktopGameClient/ServiceAcces/GameTableServiceAccess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesktopGameClient.GameTableServiceReference;
+using DesktopGameClient.Models;
+
+namespace DesktopGameClient.ServiceAcces {
+    public class GameTableServiceAccess {
+        public List<GameTableModel> GetAll() {
+            using (GameTableManagementServiceClient proxy = new GameTableManagementServiceClient()) {
+                var serviceGameTables = proxy.GetAll();
+                List<Models.GameTableModel> clientGameTables = GameTableModelConverter.ConvertFromServiceGameTablesToClientGameTables(serviceGameTables);
+                return clientGameTables;
+            }
+        }
+
+        public  GameTableModel GetById(int tableId) {
+            using (GameTableManagementServiceClient proxy = new GameTableManagementServiceClient()) {
+                var serviceGameTable = proxy.GetGameTableById(tableId);
+                Models.GameTableModel clientGameTable = GameTableModelConverter.ConvertFromServiceGameTableToClientGameTable(serviceGameTable);
+                return clientGameTable;
+            }
+        }
+
+        public bool Delete(int tableId) {
+            using (GameTableManagementServiceClient proxy = new GameTableManagementServiceClient()) {
+                return proxy.DeleteGameTable(tableId);
+            }
+        }
+    }
+}
