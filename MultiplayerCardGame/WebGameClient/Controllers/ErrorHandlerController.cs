19155ef5b318c78a8e6811f8056fd897106200dd
Emil Hedeholm Sørensen <1074195@ucn.dk>
2019-12-05 12:40:16 +0100

    started doing VERY simple error handling in the mvc client

---
+++ MultiplayerCardGame/WebGameClient/Controllers/ErrorHandlerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebGameClient.Controllers
+{
+    public class ErrorHandlerController : Controller
+    {
+        // GET: ErrorHandler
+        public ActionResult Index()
+        {
+            return RedirectToAction("Error", new { id = 1});
+        }
+
+        public ActionResult Error(int id) {
+            switch (id) {
+                case 2:
+                    ViewBag.Message = "Sorry an error ocurred while trying to start your game";
+                    break;
+                default:
+                    ViewBag.Message = "Sorry an error ocurred";
+                    break;
+            }
+            return View();
+        }
+    }
+}
