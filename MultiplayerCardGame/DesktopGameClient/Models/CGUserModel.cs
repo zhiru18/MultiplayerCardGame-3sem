85b1ad2ca76f95da053d11c1d9c130638d6a4018
Zhiru Cao <1074160@ucn.dk>
2019-12-05 11:37:32 +0100

    models convert

---
+++ MultiplayerCardGame/DesktopGameClient/Models/CGUserModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopGameClient.Models
+{
+    public class CGUserModel
+    {
+        public enum UserType
+        {
+            PLAYER,
+            ADMIN
+        };
+        public enum UserStatus
+        {
+            UNATTACKABLE,
+            ATTACKABLE,
+            STUNNED,
+            INGAME
+        };
+        public  String Id { get; set; }
+        public String UserName { get; set; }
+        public String Email { get; set; }
+        public int Health { get; set; }
+        public UserType userType { get; set; }
+        public UserStatus userStatus { get; set; }
+
+        public CGUserModel(string userName, string email, UserType userType, UserStatus userStatus)
+        {
+            this.UserName = userName;
+            this.Email = email;
+            this.userType = userType;
+            this.userStatus = userStatus;
+        }
+        public CGUserModel()
+        {
+        }
+
+        public override string ToString()
+        {
+            return null;
+        }
+
+    }
+}
---
+++ MultiplayerCardGame/DesktopGameClient/ServiceAcces/GameTableModelConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesktopGameClient.GameTableServiceReference;
+using DesktopGameClient.Models;
+
+namespace DesktopGameClient.ServiceAcces
+{
+    public class GameTableModelConverter
+    {
+        internal static Models.GameTableModel ConvertFromServiceGameTableToClientGameTable(GameTableServiceReference.GameTable serviceGameTable)
+        {
+            Models.GameTableModel clientGameTable = new Models.GameTableModel() {
+                Id = serviceGameTable.Id,
+                TableName = serviceGameTable.TableName,
+                Users = ConvertFromServiceListOfUsersToClientListOfUsers(serviceGameTable.Users),              
+            };
+            return clientGameTable;
+        }
+
+        private static List<Models.CGUserModel> ConvertFromServiceListOfUsersToClientListOfUsers(GameTableServiceReference.CGUser[] serviceUsers) {
+            List<Models.CGUserModel> clientUsers = new List<Models.CGUserModel>();
+            for (int i = 0; i < serviceUsers.Length; i++) {
+                Models.CGUserModel clientUser = ConvertFromServiceUserToClientUser(serviceUsers[i]);
+                clientUsers.Add(clientUser);
+            }
+            return clientUsers;
+        }
+
+        public static Models.CGUserModel ConvertFromServiceUserToClientUser(GameTableServiceReference.CGUser serviceUser) {
+            Models.CGUserModel clientUser = new Models.CGUserModel() {
+                Id = serviceUser.Id,
+                UserName = serviceUser.UserName,
+                Email = serviceUser.Email,
+                userType = (Models.CGUserModel.UserType)serviceUser.userType,
+                userStatus = (Models.CGUserModel.UserStatus)serviceUser.userStatus,
+                Health = serviceUser.Health,              
+            };
+            return clientUser;
+        }
+        public static List<Models.GameTableModel> ConvertFromServiceGameTablesToClientGameTables(IEnumerable<GameTableServiceReference.GameTable> serviceGameTables)
+        {
+            List<Models.GameTableModel> clientGameTables = new List<Models.GameTableModel>();
+            foreach (GameTableServiceReference.GameTable sgt in serviceGameTables) {
+                Models.GameTableModel mgt = ConvertFromServiceGameTableToClientGameTable(sgt);
+                clientGameTables.Add(mgt);
+            }
+            return clientGameTables;
+        }
+    }
+}
