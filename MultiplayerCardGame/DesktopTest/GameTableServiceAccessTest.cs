a2da3a848d9ae771ed8d68c63618296dc5d021cd
Zhiru Cao <1074160@ucn.dk>
2019-12-06 11:06:53 +0100

    service test

---
+++ MultiplayerCardGame/DesktopTest/GameTableServiceAccessTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesktopGameClient.Models;
+using DesktopGameClient.ServiceAcces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesktopTest.GameTableServiceReference;
+
+namespace DesktopTest {
+    [TestClass]
+    public class GameTableServiceAccessTest {
+        [TestMethod]
+        public void GetAllTest() {
+            //Arrange
+            GameTableServiceAccess gameTableServiceAccess = new GameTableServiceAccess();
+
+            //Act
+            List<GameTableModel> gameTables = gameTableServiceAccess.GetAll();
+
+            //Assert
+            Assert.IsTrue(gameTables.Count > 0);
+        }
+    }
+}
