53485b438d0c0d226ec7dde07e881b9dc8a9d9e0
Emil Hedeholm Sørensen <1074195@ucn.dk>
2019-12-09 14:55:08 +0100

    alle metoder i cardDB bliver testet

--- MultiplayerCardGame/DataTest/CardDBTest.cs
+++ MultiplayerCardGame/DataTest/CardDBTest.cs
@@ -46,23 +46,49 @@ namespace Tests.DataTest
            List<CardModel> cardList = (List<CardModel>)cardDB.GetCardsByDeckId(2);
             Assert.IsTrue(cardList.Count > 0);
         }
-        // TODO: Fix this method.
-        //[TestMethod]
-        //public void UpdateTest()
-        //{
-        //    cardDB = new CardDB();
-        //    var testCard = new Card(Card.CardType.DEFENSE, "Attack UpdateTest card", "It can attack", 2);
-        //    Card compareCard = null;
-        //    cardDB.Insert(testCard);
-        //    testCard.Name = "Hans";
-        //    cardDB.Update(testCard);
-        //    List<Card> updatedCardList = (List<Card>)cardDB.GetAll();
-        //    foreach (var card in updatedCardList) {
-        //        if (card.Name == testCard.Name) {
-        //            compareCard = card;
-        //        }
-        //    }
-        //    Assert.AreEqual(testCard.Name, compareCard.Name);
-        //}
+
+        [TestMethod]
+        public void GetCardsByUserIdTest() {
+            cardDB = new CardDB();
+            var testCard1 = new CardModel(CardModel.CardType.DEFENSE, "Attack UpdateTest card", "It can attack", 2);
+            var testCard2 = new CardModel(CardModel.CardType.DEFENSE, "Attack UpdateTest card", "It can attack", 2);
+            var testCard3 = new CardModel(CardModel.CardType.DEFENSE, "Attack UpdateTest card", "It can attack", 2);
+            var testCard4 = new CardModel(CardModel.CardType.DEFENSE, "Attack UpdateTest card", "It can attack", 2);
+            var testCard5 = new CardModel(CardModel.CardType.DEFENSE, "Attack UpdateTest card", "It can attack", 2);
+            testCard1.Id = cardDB.InsertWithIdentity(testCard1);
+            testCard2.Id = cardDB.InsertWithIdentity(testCard2);
+            testCard3.Id = cardDB.InsertWithIdentity(testCard3);
+            testCard4.Id = cardDB.InsertWithIdentity(testCard4);
+            testCard5.Id = cardDB.InsertWithIdentity(testCard5);
+            List<CardModel> cards = new List<CardModel>() { testCard1, testCard2, testCard3, testCard4, testCard5};
+            ICGUserDBIF userDB = new CGUserDB();
+            CGUserModel user = userDB.GetById("Test");
+            userDB.InsertHand(cards, user);
+            List<CardModel> cardList = (List<CardModel>)cardDB.GetCardsByUserId("Test");
+            Assert.IsTrue(cardList.Count > 0);
+            userDB.DeleteHand(user);
+            cardDB.Delete(testCard1);
+            cardDB.Delete(testCard2);
+            cardDB.Delete(testCard3);
+            cardDB.Delete(testCard4);
+            cardDB.Delete(testCard5);
+        }
+
+       [TestMethod]
+        public void UpdateTest() {
+            cardDB = new CardDB();
+            var testCard = new CardModel(CardModel.CardType.DEFENSE, "Attack UpdateTest card", "It can attack", 2);
+            CardModel compareCard = null;
+            testCard.Id = cardDB.InsertWithIdentity(testCard);
+            testCard.Name = "Hans";
+            cardDB.Update(testCard);
+            List<CardModel> updatedCardList = (List<CardModel>)cardDB.GetAll();
+            foreach (var card in updatedCardList) {
+                if (card.Name == testCard.Name) {
+                    compareCard = card;
+                }
+            }
+            Assert.AreEqual(testCard.Name, compareCard.Name);
+        }
     }
 }
