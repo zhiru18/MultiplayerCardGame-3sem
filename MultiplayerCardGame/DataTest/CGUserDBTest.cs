f7ddbcfefa35e02c2cb25f2f44c5abbdb68366ea
Peanguin <Peanguin@users.noreply.github.com>
2019-12-10 13:31:48 +0100

    Added Tests to CGUserDBTest

--- MultiplayerCardGame/DataTest/CGUserDBTest.cs
+++ MultiplayerCardGame/DataTest/CGUserDBTest.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Server.Converters.DataContractConverters;
 using Server.Data.Data;
+using Server.DataContracts.DataContracts;
 using Server.Model.Model;
 using System;
 using System.Collections.Generic;
@@ -78,5 +80,88 @@ namespace Tests.DataTest {
             bob = cgUserDB.GetById("asdfg09876");
             Assert.IsNull(bob);
         }
+        [TestMethod]
+        public void GetUserTableIdTest()
+        {
+            cgUserDB = new CGUserDB();
+            CGUserModel testUser = cgUserDB.GetById("Test");
+            var attempt = cgUserDB.GetUserTableId("Test");
+            Assert.AreEqual(attempt, testUser.TableID);
+        }
+        [TestMethod]
+        public void GetByUserNameTest() 
+        {
+            cgUserDB = new CGUserDB();
+            CGUserModel testUser = cgUserDB.GetByUserName("Test");
+            Assert.AreEqual("Test", testUser.UserName);
+        }
+        [TestMethod]
+        public void InsertHandTest()
+        {
+            //Arrange
+            cgUserDB = new CGUserDB();
+            CardDB cardDB = new CardDB();
+            //Act
+            var userModel = cgUserDB.GetById("Test");
+            List<CardModel> cardsOnHand = cardDB.GetCardsByDeckId(2);
+            List<CardModel> filteredCardsOnHand = new List<CardModel>();
+            for (int i = 0; i < 5; i++)
+            {
+                CardModel card = cardsOnHand[i];
+                filteredCardsOnHand.Add(card);
+            }
+            cgUserDB.InsertHand(filteredCardsOnHand, userModel);
+            CGUser user = CGUserConverter.convertFromCGUserModelToCGUser(cgUserDB.GetById("Test"));
+            //Assert
+            Assert.IsTrue(user.cards.Count > 0);
+            //CleanUp
+            CGUserModel revertedUser = CGUserConverter.ConvertFromCGUserToCGUserModel(user);
+            cgUserDB.DeleteHand(revertedUser);
+        }
+
+        [TestMethod]
+        public void DeleteHandTest() 
+        {
+            //Arrange
+            cgUserDB = new CGUserDB();
+            CardDB cardDB = new CardDB();
+            //Act
+            var userModel = cgUserDB.GetById("Test");
+            List<CardModel> cardsOnHand = cardDB.GetCardsByDeckId(2);
+            List<CardModel> filteredCardsOnHand = new List<CardModel>();
+            for (int i = 0; i < 5; i++)
+            {
+                CardModel card = cardsOnHand[i];
+                filteredCardsOnHand.Add(card);
+            }
+            cgUserDB.InsertHand(filteredCardsOnHand, userModel);
+            cgUserDB.DeleteHand(userModel);
+            CGUser user = CGUserConverter.convertFromCGUserModelToCGUser(cgUserDB.GetById("Test"));
+            //Assert
+            Assert.IsTrue(user.cards.Count == 0);
+        }
+        
+        [TestMethod]
+        public void GetUserByTableIdTest()
+        {
+            //Arrange
+            cgUserDB = new CGUserDB();
+            GameTableDB gameTableDB = new GameTableDB();
+            var table = new GameTableModel("TestTable");
+            table.DeckId = 2;
+            table.seats = 4;
+            //Act
+            gameTableDB.Insert(table);
+            table = gameTableDB.GetGameTableByTableName("TestTable");
+            GameTable gameTable = GameTableConverter.ConvertFromGameTableModelToGameTable(table);
+            gameTable.Users.Add(CGUserConverter.convertFromCGUserModelToCGUser(cgUserDB.GetById("Test")));
+            CGUserModel testUser = cgUserDB.GetById("Test");
+            cgUserDB.UpdateUserTableId(testUser, table.Id);
+            List<CGUserModel> foundUser = cgUserDB.GetUserByTableId(table.Id);
+            Assert.AreEqual(testUser.UserName, foundUser[0].UserName);
+            gameTableDB.Delete(table);
+                
+        }
+        
     }
 }
