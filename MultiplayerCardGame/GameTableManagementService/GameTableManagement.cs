1a6004e84f63929e28066f14ce41adfe3c2dc7ea
Morten <morten.m.kristensen@gmail.com>
2019-12-11 11:11:43 +0100

    Fixed the way we handle seats on the table a user is leaving.

--- MultiplayerCardGame/GameTableManagementService/GameTableManagement.cs
+++ MultiplayerCardGame/GameTableManagementService/GameTableManagement.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Threading;
 using System.Transactions;
 using Server.Converters.DataContractConverters;
 using Server.Data.Data;
@@ -69,24 +70,27 @@ namespace Server.Services.GameTableManagementService {
                 throw new ArgumentNullException();
             } else {
                 GameTable databaseTable = null;
+                GameTableModel modelTable = null;
                 try {
                     using (TransactionScope scope = new TransactionScope()) {
+                        
                         CGUserModel userModel = CGUserConverter.ConvertFromCGUserToCGUserModel(user);
                         if (userModel.TableID != 0 && userModel.TableID != chosenTable.Id) {
-                            GameTableModel modelTable = gameTableDB.GetById(userModel.TableID);
-                            if(modelTable != null) {
-                                gameTableDB.UpdateGameTableSeats(modelTable, -1);
-                            }
+                            modelTable = gameTableDB.GetById(userModel.TableID);
                         }
                         databaseTable = GameTableConverter.ConvertFromGameTableModelToGameTable(gameTableDB.GetById(chosenTable.Id));
                         if (chosenTable.seats == databaseTable.seats && databaseTable.Users.Count < 4) {
                             userManagement.UpdateUserTableId(user, databaseTable.Id);
                             databaseTable.Users.Add(user);
                             UpdateGameTableSeats(databaseTable, 1);
+                            if (modelTable != null) {
+                                gameTableDB.UpdateGameTableSeats(modelTable, -1);
+                            }
                         }
+                        Thread.Sleep(2000);
                         scope.Complete();
                     }
-                } catch (TransactionAbortedException tae) {
+                } catch (Exception e) {
 
                     throw;
                 }
